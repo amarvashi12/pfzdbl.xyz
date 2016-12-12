7b5f1890e03cf9f3fa2a6f64c32e07f3a9fa13c8
Dav Glass <dav.glass@yahoo.com>
2016-12-12 18:23:33 +0530

    added database details
    add database

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'demo-project');
+define('DB_NAME', 'database_staging');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'db_patil');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'admin12345');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
